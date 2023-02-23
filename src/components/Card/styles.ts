8268bd42b2d876ac2cd734d8c37a146f2225bbc6
claytonfortunato <claytonreis10@gmail.com>
2023-02-22 22:51:16 -0300

    feat: style card

--- src/components/Card/styles.ts
+++ src/components/Card/styles.ts
@@ -4,9 +4,18 @@ import theme from "../../theme";
 export const Container = styled.div``;
 
 export const Content = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
   position: relative;
   width: 200px;
   height: 200px;
+  transform-style: preserve-3d;
+  transition: transform 600ms ease-in-out;
+  &:hover {
+    transform: rotateY(180deg);
+  }
 `;
 
 export const Front = styled.div`
@@ -15,13 +24,13 @@ export const Front = styled.div`
   height: 100%;
   background-color: ${theme.colors.primary};
   color: ${theme.colors.light};
-
-  &:hover {
-    transform: rotateY(180deg);
-  }
+  backface-visibility: hidden;
 `;
 
 export const Back = styled.div`
   background-color: ${theme.colors.secondary};
   color: ${theme.colors.dark};
+  transform: rotateY(180deg);
+  width: 200px;
+  height: 200px;
 `;
